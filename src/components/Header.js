54d7bd5e88f14a85816c6f022fdee95036d2606e
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-03-28 01:07:01 -0400

    testing more

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <div className="header">
             <header>
                 <img src="https://www.dcmax1k.tk/images/Dcmax1klogo-round.png" alt="Logo" height="40px" width="40px" />
-                <h1>DCmax1k</h1>
+                <h1>Testing</h1>
             </header>
         </div>
     )
