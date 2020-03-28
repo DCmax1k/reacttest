11a1d41d2ceeeab7ccbf9f49c3404500cb83ba88
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-03-28 01:48:28 -0400

    New styling

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div className="nav-links">
+            <ul>
+                <li>Home</li>
+                <li>Are you?</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
