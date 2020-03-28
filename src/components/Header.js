272880b93b586646762b2b01a1981edcf40c684d
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-03-28 00:59:25 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <header>
+                <img src="https://www.dcmax1k.tk/images/Dcmax1klogo-round.png" alt="Logo" height="40px" width="40px" />
+                <h1>DCmax1k</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
